d539fb678af95f04942e19fb3426537ce87d3d67
l-am-Chinese <897731578@qq.com>
2022-01-26 10:21:30 +0800

    update dependencies

--- src/main/java/com/zhs/MyBlogApplication.java
+++ src/main/java/com/zhs/MyBlogApplication.java
@@ -6,8 +6,10 @@ import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -19,8 +21,11 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableTransactionManagement
 @EnableWebMvc
 @EnableScheduling
-public class MyBlogApplication {
-
+public class MyBlogApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(MyBlogApplication.class);
+	}
 	public static void main(String[] args) {
 
 		SpringApplication.run(MyBlogApplication.class, args);
