f0f8cd7d958fe76c5be28d4c3acd72b7e40c8c59
l-am-Chinese <897731578@qq.com>
2022-02-09 15:53:38 +0800

    小修改

--- src/main/java/com/zhs/constant/OSSClientConstants.java
+++ src/main/java/com/zhs/constant/OSSClientConstants.java
@@ -14,7 +14,7 @@ public class OSSClientConstants {
     /**
      * 阿里云API的外网域名
      */
-    public static final String ENDPOINT = "oss-cn-shenzhen.aliyuncs.com";
+    public static final String ENDPOINT = "oss-cn-shanghai.aliyuncs.com";
 
     /**
      * 阿里云API的密钥Access Key ID
@@ -29,7 +29,7 @@ public class OSSClientConstants {
      * 阿里云API的bucket名称
      * 在阿里云上自己创建一个bucket
      */
-    public static final String BACKET_NAME = "zhy-myblog";
+    public static final String BACKET_NAME = "zhs-myblog";
 
     /**
      * 阿里云API的文件夹名称
--- src/main/resources/static/js/editormd/editor.js
+++ src/main/resources/static/js/editormd/editor.js
@@ -281,7 +281,7 @@
                     } else if(data['status'] == 103) {
                         alert(data['message'] + " 发表文章失败")
                     } else if(data['status'] == 204) {
-                        alert("发布失败了，都叫你不要发布了，不听嘛")
+                        alert("发布失败了，因为你不是管理员")
                     } else {
                         $('#my-alert').modal('close');
                         window.removeEventListener('beforeunload',fnClose);
